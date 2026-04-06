e75fdb94dc591d3b5dd1a346d4b1117a142465ff
Franz Seidel <franz8818@gmail.com>
2026-04-06 15:58:36 -0500

    Commit para prevenir errores por cambio de estructura

---
+++ app/exercises.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+export default function exercisesScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Pantalla de Ejercicios</Text>
+    </View>
+  );
+}
