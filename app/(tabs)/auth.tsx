7d30bc140f47c7e55603c5a1c41273fee1dca75b
franzseidel <franz8818@gmail.com>
2026-04-15 11:06:33 -0500

    refactor: mejora la estructura de AuthScreen y añade encabezado con logo

--- app/(tabs)/auth.tsx
+++ app/(tabs)/auth.tsx
@@ -1,35 +1,53 @@
 import { StyleSheet, TextInput } from 'react-native';
+import { Image } from 'expo-image';
+
 
 import { ThemedText } from '@/components/themed-text';
 import { ThemedView } from '@/components/themed-view';
 import { Fonts } from '@/constants/theme';
 
+import { useState } from 'react';
+
 export default function AuthScreen() {
+  const [isRegister, setIsRegister] = useState(false);
   return (
-    <ThemedView style={styles.container}>
-      
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText
-          type="title"
-          style={{ fontFamily: Fonts.rounded }}>
-          Iniciar sesión
-        </ThemedText>
+
+    <ThemedView style={{ flex: 1 }}>
+
+   <ThemedView style={styles.header}>
+  <Image
+    source={require('@/assets/images/FitnessApp-2.png')}
+    style={styles.fitnessAppLogo}
+  />
+</ThemedView>
+
+
+      <ThemedView style={styles.container}>
+
+        <ThemedView style={styles.titleContainer}>
+          <ThemedText
+            type="title"
+            style={{ fontFamily: Fonts.rounded }}>
+            {isRegister ? 'Crear cuenta' : 'Iniciar sesión'}
+          </ThemedText>
+        </ThemedView>
+
+        <ThemedText>Ingresa tus datos para continuar</ThemedText>
+
+        <TextInput
+          placeholder="Correo electrónico"
+          style={styles.input}
+        />
+
+        <TextInput
+          placeholder="Contraseña"
+          secureTextEntry
+          style={styles.input}
+        />
+
       </ThemedView>
-
-      <ThemedText>Ingresa tus datos para continuar</ThemedText>
-
-      <TextInput
-        placeholder="Correo electrónico"
-        style={styles.input}
-      />
-
-      <TextInput
-        placeholder="Contraseña"
-        secureTextEntry
-        style={styles.input}
-      />
-
-    </ThemedView>
+      </ThemedView>
+   
   );
 }
 
@@ -39,6 +57,14 @@ const styles = StyleSheet.create({
     padding: 20,
     justifyContent: 'center',
   },
+  header: {
+  width: '100%',
+  height: 250,
+  justifyContent: 'center',
+  alignItems: 'center',
+  backgroundColor: '#f2f2f2', // 👈 simula el gris
+},
+
   titleContainer: {
     flexDirection: 'row',
     marginBottom: 20,
@@ -50,5 +76,11 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     paddingHorizontal: 15,
     marginTop: 15,
+  },
+  fitnessAppLogo: {
+    width: 200,
+    height: 200,
+    alignSelf: 'center',
+    marginTop: 60,
   },
 });
